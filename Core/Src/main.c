2d67f02db93b066141ebac19798ed0c1f3eb3ab0
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2020-03-31 21:50:05 +0200

    Dodanie plikow i konfiguracji do obslugi LCD

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,8 +21,13 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "adc.h"
+#include "dma2d.h"
+#include "i2c.h"
+#include "ltdc.h"
+#include "spi.h"
 #include "tim.h"
 #include "gpio.h"
+#include "fmc.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -98,6 +103,11 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM4_Init();
   MX_ADC1_Init();
+  MX_DMA2D_Init();
+  MX_FMC_Init();
+  MX_I2C3_Init();
+  MX_LTDC_Init();
+  MX_SPI5_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
   HAL_ADC_Start(&hadc1);
@@ -109,24 +119,26 @@ int main(void)
   {
 	if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
 		sensorReadValue[0] = HAL_ADC_GetValue(&hadc1);
-		ADC_SetActiveChannel(&hadc1, ADC_CHANNEL_11);
-		HAL_ADC_Start(&hadc1);
-	}
-	if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
-		sensorReadValue[1] = HAL_ADC_GetValue(&hadc1);
 		ADC_SetActiveChannel(&hadc1, ADC_CHANNEL_12);
 		HAL_ADC_Start(&hadc1);
 	}
 	if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
-		sensorReadValue[2] = HAL_ADC_GetValue(&hadc1);
+		sensorReadValue[1] = HAL_ADC_GetValue(&hadc1);
 		ADC_SetActiveChannel(&hadc1, ADC_CHANNEL_13);
 		HAL_ADC_Start(&hadc1);
 	}
+	if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
+		sensorReadValue[2] = HAL_ADC_GetValue(&hadc1);
+		ADC_SetActiveChannel(&hadc1, ADC_CHANNEL_11);
+		HAL_ADC_Start(&hadc1);
+	}
+	/*
 	if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
 		sensorReadValue[3] = HAL_ADC_GetValue(&hadc1);
 		ADC_SetActiveChannel(&hadc1, ADC_CHANNEL_10);
 		HAL_ADC_Start(&hadc1);
 	}
+	*/
 
     /* USER CODE END WHILE */
 
@@ -143,11 +155,12 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage 
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
   /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
@@ -156,10 +169,16 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 180;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Activate the Over-Drive mode 
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
@@ -172,7 +191,15 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV8;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 216;
+  PeriphClkInitStruct.PLLSAI.PLLSAIR = 2;
+  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
