437f68a89aaed1d4ef1f368addef39a9148195c2
Wirgiliusz <kamx98@gmail.com>
2020-06-07 18:47:26 +0200

    Dodanie czesci projektu z wizualizacji RVIZ oraz schematu elektronicznego

---
+++ ROS_RVIZ/catkin_ws/build/ros_robotics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/r0ck/catkin_ws/src/ros_robotics
+# Build directory: /home/r0ck/catkin_ws/build/ros_robotics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
